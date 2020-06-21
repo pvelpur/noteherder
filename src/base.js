ce91897f217392fab4e100e8e3b5cb0f6ecd0542
prithdamyth <pvelpur@purdue.edu>
2020-06-21 10:57:03 -0500

    adding base.js to gitignore

--- src/base.js
+++ src/base.js
@@ -2,7 +2,14 @@ import Rebase from 're-base'
 import firebase from 'firebase/app'
 import database from 'firebase/database'
 
-const app = firebase.initializeApp()
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyAYHm0JbojejaiG-pTLDtQRXMB_N0-inu8",
+    authDomain: "noteherder-128d1.firebaseapp.com",
+    databaseURL: "https://noteherder-128d1.firebaseio.com",
+    projectId: "noteherder-128d1",
+    storageBucket: "noteherder-128d1.appspot.com",
+    messagingSenderId: "988280756198",
+  })
 
 const db = firebase.database(app)
 
